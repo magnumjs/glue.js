bad66a89b2252697ecb37810b79e9dc111247a49
magnumjs <mglazer@gmail.com>
2013-08-09 13:30:44 -0400

    Update
    adding real world working example

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
-var TodosDetails = function() {
-  var TodosDetails = function(data) {
+var TodosDetails=function(){
+  TodosDetails = function(data) {
 		$.extend(this, data);
 	};
 
 	TodosDetails.get = function(id) {
-		return $.get('/todos-details').then(
+		return $.get('http://api.openkeyval.org/some-arbitrary-key').then(
 				function(response) {
 					return new TodosDetails(response);
 				});
@@ -12,3 +12,13 @@ var TodosDetails = function() {
 
 	return TodosDetails;
 };
+
+var converter=function(d){
+  return jQuery.parseJSON(d);
+};
+$.ajaxSetup({crossDomain:true,dataType:'jsonp',jsonpCallback: 'converter'});
+
+mag.service(['TodosDetails'],function(d){
+ $('div').text(d.items[0].sample);
+});
+  
