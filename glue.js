a620ec58657595506ca5a62f8b4f73fd7c396c0b
magnumjs <magnumjs@users.noreply.github.com>
2013-08-10 16:15:05 -0400

    Update glue.js

--- glue.js
+++ glue.js
@@ -1,55 +1,50 @@
-'use strict';
-
 //////////////////////////////////
 //GlueJS
 //////////////////////////////////
 
-/**
- * @ngdoc function
- * @name mag.glue
- * @function
+/*!
+ * MagnumJS - Core Control Factory v0.11.1
+ * https://github.com/magnumjs
  *
- * @description
- * Wraps a data model into a parameter
+ * Includes Staples.js & Glue.JS
+ * https://github.com/magnumjs/mag.js
  *
+ * Copyright 2013 Michael GLazer 
+ * Released under the MIT license
+ * https://github.com/magnumjs/mag.js/blob/master/LICENSE
+ *
+ * Date: 2013-08-10T13:48Z
  */
  
- 
- var $service = (function($) {
-  
-	// shortcuts to available services
-	
-	// load a preconfigured namespace for a service model -> to combine a service response to a model object
+ 'use strict';
 
-	var cachedScriptPromises = {};
-	_cachedGet = function( name ) {
-		if ( !cachedScriptPromises[ name ] ) {
-			cachedScriptPromises[ name ] = $.Deferred(function( defer ) {
-				window[name]().get().then( defer.resolve, defer.reject );
-			}).promise();
-		}
-		return cachedScriptPromises[ name ];
-	};
-	
-	var	service=function(serviceNamesArray,callback,errorHandler){
-		var newList=[];
-		// if cache is configured
-		for(var i=0;i<serviceNamesArray.length;i++){
-			newList[i]=	_cachedGet (serviceNamesArray[i]);
-		}
-		$.when.apply($, newList)
-		.done(function(){
-				callback.apply(this,arguments);
-			}
-		})
-		.fail(function(a){
-			errorHandler.apply(this,arguments);
-		});
-	};
-	
-	
-	return {
-		serve:service
-	};
-	
-}(jQuery));
+mag.service = function (serviceNamesArray, callback, errorHandler) {
+
+
+    var cachedScriptPromises = {};
+    _cachedGet = function (name) {
+        if (!cachedScriptPromises[name]) {
+            cachedScriptPromises[name] = $.Deferred(function (defer) {
+                window[name]().get().then(defer.resolve, defer.reject);
+            }).promise();
+        }
+        return cachedScriptPromises[name];
+    };
+
+
+    var newList = [];
+    // if cache is configured
+    for (var i = 0; i < serviceNamesArray.length; i++) {
+        newList[i] = _cachedGet(serviceNamesArray[i]);
+    }
+    $.when.apply($, newList)
+        .done(function () {
+                callback.apply(this, arguments);
+            }
+    })
+    .fail(function (a) {
+        errorHandler.apply(this, arguments);
+    });
+
+
+};
